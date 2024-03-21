cd2de3ae373ed51caba9298b028e09520541bbaa
ro <reneosorio77@gmail.com>
2024-03-21 12:14:00 +0900

    Average Time of Process per Machine query

--- rising_temperature.sql
+++ rising_temperature.sql
@@ -1,4 +1,9 @@
--- Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+-- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+
+-- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+-- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
 
 -- Return the result table in any order.
 
@@ -9,23 +14,47 @@
 -- Example 1:
 
 -- Input:
--- Weather table:
--- +----+------------+-------------+
--- | id | recordDate | temperature |
--- +----+------------+-------------+
--- | 1  | 2015-01-01 | 10          |
--- | 2  | 2015-01-02 | 25          |
--- | 3  | 2015-01-03 | 20          |
--- | 4  | 2015-01-04 | 30          |
--- +----+------------+-------------+
+-- Activity table:
+-- +------------+------------+---------------+-----------+
+-- | machine_id | process_id | activity_type | timestamp |
+-- +------------+------------+---------------+-----------+
+-- | 0          | 0          | start         | 0.712     |
+-- | 0          | 0          | end           | 1.520     |
+-- | 0          | 1          | start         | 3.140     |
+-- | 0          | 1          | end           | 4.120     |
+-- | 1          | 0          | start         | 0.550     |
+-- | 1          | 0          | end           | 1.550     |
+-- | 1          | 1          | start         | 0.430     |
+-- | 1          | 1          | end           | 1.420     |
+-- | 2          | 0          | start         | 4.100     |
+-- | 2          | 0          | end           | 4.512     |
+-- | 2          | 1          | start         | 2.500     |
+-- | 2          | 1          | end           | 5.000     |
+-- +------------+------------+---------------+-----------+
 -- Output:
--- +----+
--- | id |
--- +----+
--- | 2  |
--- | 4  |
--- +----+
-SELECT w1.id AS  id
-FROM Weather w1
-JOIN Weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+-- +------------+-----------------+
+-- | machine_id | processing_time |
+-- +------------+-----------------+
+-- | 0          | 0.894           |
+-- | 1          | 0.995           |
+-- | 2          | 1.456           |
+-- +------------+-----------------+
+-- Explanation:
+-- There are 3 machines running 2 processes each.
+-- Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
+-- Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
+-- Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
+SELECT machine_id,
+       ROUND(AVG(end_time-start_time),3) AS processing_time
+FROM (
+        SELECT a1.machine_id AS machine_id,
+                a1.timestamp AS start_time,
+                a2.timestamp AS end_time
+        FROM Activity a1
+        JOIN Activity a2 ON a1.machine_id = a2.machine_id
+                        AND a1.process_id = a2.process_id
+                        AND a1.activity_type = 'start'
+                        AND a2.activity_type = 'end'
+        WHERE a2.timestamp > a1.timestamp
+        GROUP BY a1.machine_id, a1.process_id) AS times
+GROUP BY machine_id;

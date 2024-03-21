75d152b00bebc30a4e0349cad427e209a78f0415
ro <reneosorio77@gmail.com>
2024-03-21 10:50:44 +0900

    customer-who-visited-but-did-not-make-any-transactions query

---
+++ replace_employeed_id copy 2.sql
@@ -0,0 +1,53 @@
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+-- Return the result table sorted in any order.
+
+-- The result format is in the following example.
+
+
+
+-- Example 1:
+
+-- Input:
+-- Visits
+-- +----------+-------------+
+-- | visit_id | customer_id |
+-- +----------+-------------+
+-- | 1        | 23          |
+-- | 2        | 9           |
+-- | 4        | 30          |
+-- | 5        | 54          |
+-- | 6        | 96          |
+-- | 7        | 54          |
+-- | 8        | 54          |
+-- +----------+-------------+
+-- Transactions
+-- +----------------+----------+--------+
+-- | transaction_id | visit_id | amount |
+-- +----------------+----------+--------+
+-- | 2              | 5        | 310    |
+-- | 3              | 5        | 300    |
+-- | 9              | 5        | 200    |
+-- | 12             | 1        | 910    |
+-- | 13             | 2        | 970    |
+-- +----------------+----------+--------+
+-- Output:
+-- +-------------+----------------+
+-- | customer_id | count_no_trans |
+-- +-------------+----------------+
+-- | 54          | 2              |
+-- | 30          | 1              |
+-- | 96          | 1              |
+-- +-------------+----------------+
+# Write your MySQL query statement below
+SELECT
+    v.customer_id,
+    COUNT(v.visit_id) AS count_no_trans
+FROM
+    Visits v
+LEFT JOIN
+    Transactions t ON v.visit_id = t.visit_id
+WHERE
+    t.transaction_id IS NULL
+GROUP BY
+    v.customer_id;
